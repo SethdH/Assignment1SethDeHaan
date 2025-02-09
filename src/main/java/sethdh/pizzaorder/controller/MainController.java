014828cd5fd2105c5c73d3744cb5129b2964dbf5
sethd <dehaanse@sheridancollege.ca>
2025-02-09 15:17:22 -0500

    Basic Navigation between all required pages

--- src/main/java/sethdh/pizzaorder/controller/MainController.java
+++ src/main/java/sethdh/pizzaorder/controller/MainController.java
@@ -1,8 +1,11 @@
 package sethdh.pizzaorder.controller;
 
-import ch.qos.logback.core.model.Model;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import sethdh.pizzaorder.model.UserOrder;
 
 
 @Controller
@@ -10,6 +13,13 @@ public class MainController {
 
     @GetMapping("/OnlinePizza")
     public String displayIndexPage(Model model) {
+        UserOrder userOrder = new UserOrder("", "", "", "");
+        model.addAttribute("userOrder", userOrder);
         return "index";
+    }
+
+    @PostMapping("/submitOrder")
+    public String submitOrder(Model model, @ModelAttribute UserOrder userOrder) {
+        return "recipt";
     }
 }
--- src/main/java/sethdh/pizzaorder/model/UserOrder.java
+++ src/main/java/sethdh/pizzaorder/model/UserOrder.java
@@ -1,7 +1,48 @@
 package sethdh.pizzaorder.model;
 
-import java.util.List;
-
 public class UserOrder {
+    String fName;
+    String lName;
+    String phoneNumber;
+    String address;
 
+    public UserOrder(String fName, String lName, String phoneNumber, String address) {
+        this.fName = fName;
+        this.lName = lName;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
+
